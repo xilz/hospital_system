a6c9d8d5882c2e250a4d15c1a3975f694da55257
XilingZHU <zxl041024@163.com>
2026-03-29 18:26:09 +0800

    H5 登录静态页面完成

--- pzH5/src/pages/Main.vue
+++ pzH5/src/pages/Main.vue
@@ -1,7 +1,27 @@
 <template>
-    layout
     <RouterView />
+    <van-tabbar v-model="active">
+        <van-tabbar-item 
+        v-for="item in router.options.routes[0].children" 
+        :key="item.path" 
+        :icon="item.meta.icon"
+        :url="`#/${item.path}`"
+        >{{ item.meta.name }}</van-tabbar-item>
+
+    </van-tabbar>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+
+const active = ref(0)
+const route = useRoute()
+const router = useRouter()
+onMounted(() => {
+    // 路由数据
+    console.log(router, 'router')
+    const data = router.options.routes[0]
+    active.value = data.children.findIndex(item => '/' + item.path === route.path)
+})
 </script>
--- pzH5/src/pages/login/index.vue
+++ pzH5/src/pages/login/index.vue
@@ -1,6 +1,49 @@
 <template>
-    <div>login</div>
+    <h1>用户登录</h1>
+    <van-form @submit="onSubmit">
+        <van-cell-group inset>
+            <van-field
+            v-model="form.userName"
+            name="username"
+            label="用户名"
+            placeholder="用户名"
+            :rules="[{ required: true, message: '请填写用户名' }]"
+            />
+            <van-field
+            v-model="form.passWord"
+            type="password"
+            name="password"
+            label="密码"
+            placeholder="密码"
+            :rules="[{ required: true, message: '请填写密码' }]"
+            />
+        </van-cell-group>
+        <div class="btn">
+            <van-button native-type="submit" round block type="primary">提交</van-button>
+        </div>
+    </van-form>
 </template>
 
 <script setup>
+import { reactive } from 'vue';
+
+// 表单数据
+const form = reactive({
+    userName: '',
+    passWord: ''
+}) 
+
+// 表单提交
+const onSubmit = () => {
+
+}
 </script>
+
+<style lang="less" scoped>
+h1 {
+    text-align: center;
+}
+.btn {
+    margin: 16px;
+}
+</style>
